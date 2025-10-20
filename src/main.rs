8fd7cbce2802fda9fd0a4a63542da65989eb4f26
drumath2237 <2237k.drumath@gmail.com>
2025-10-21 05:23:09 +0900

    :sparkles: find metajson file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
 use std::{fs::File, io::Read};
 
+use zip::result::ZipError;
+
+use crate::zip_test::SogFile;
+
 mod zip_test;
 
-fn main() {
+fn main() -> Result<(), ZipError> {
     let mut file = File::open("./sample_data/pizza.sog").unwrap();
     let mut bytes = Vec::new();
     let _size = file.read_to_end(&mut bytes).unwrap();
-    zip_test::zip_test(&bytes)
+    let files = zip_test::zip_test(&bytes)?;
+
+    let sog_file = files
+        .iter()
+        .find(|f| match f {
+            SogFile::MetaJson(_) => true,
+            SogFile::Image {
+                data: _,
+                filename: _,
+            } => false,
+        })
+        .ok_or(ZipError::FileNotFound)?;
+
+    match sog_file {
+        SogFile::MetaJson(json) => println!("{}", json),
+        SogFile::Image {
+            filename: _,
+            data: _,
+        } => println!("no"),
+    }
+
+    Ok(())
 }
--- src/zip_test.rs
+++ src/zip_test.rs
@@ -1,14 +1,44 @@
 use std::io::Cursor;
+use std::io::Read;
 
-use zip::ZipArchive;
+use zip::{ZipArchive, result::ZipError};
 
-pub fn zip_test(zip_bytes: &[u8]) -> () {
+pub struct RawFile {
+    pub name: String,
+    pub data: Vec<u8>,
+}
+
+pub enum SogFile {
+    MetaJson(String),
+    Image { filename: String, data: Vec<u8> },
+}
+
+pub fn zip_test(zip_bytes: &[u8]) -> Result<Vec<SogFile>, ZipError> {
     let cursor = Cursor::new(zip_bytes);
 
-    let mut archive = ZipArchive::new(cursor).unwrap();
+    let mut archive = ZipArchive::new(cursor)?;
+    let mut files = Vec::new();
 
     for i in 0..archive.len() {
-        let file = archive.by_index(i).unwrap();
-        println!("{}", &file.name())
+        let mut zip_file = archive.by_index_raw(i)?;
+        // raw_files.push(RawFile { name, data: buf });
+        let name = zip_file.name().to_owned();
+
+        let sog = if name == "meta.json" {
+            let mut buf = String::new();
+            zip_file.read_to_string(&mut buf)?;
+            SogFile::MetaJson(buf)
+        } else {
+            let mut buf = Vec::new();
+            let _size = zip_file.read_to_end(&mut buf)?;
+            SogFile::Image {
+                filename: name,
+                data: buf,
+            }
+        };
+
+        files.push(sog);
     }
+
+    Ok(files)
 }
