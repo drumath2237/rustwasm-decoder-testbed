9c1662cc51c47a49b33a30dc1d3a21f323332539
drumath2237 <2237k.drumath@gmail.com>
2025-10-30 06:00:34 +0900

    :bug: load wasm code
    it has error

--- wasm-test/src/main.ts
+++ wasm-test/src/main.ts
@@ -1,7 +1,17 @@
 import "./style.css";
 
-function main() {
-  console.log("hi");
+import { zip_test_wasm } from "./wasm/sog-wasm";
+
+import sogPath from "../../sample_data/pizza.sog?url";
+
+async function main() {
+  fetch(sogPath).then((res) => res.arrayBuffer()).then((data) => {
+    const sogData = new Uint8Array(data);
+    console.log(sogData.length);
+
+    const files = zip_test_wasm(sogData);
+    console.log(files);
+  });
 }
 
 main();
