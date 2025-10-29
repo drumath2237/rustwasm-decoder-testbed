5610bdea65dcc5971f9bf0e577572e34a45da0a1
drumath2237 <2237k.drumath@gmail.com>
2025-10-30 05:30:56 +0900

    :sparkles: wasm test method

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # rustwasm-decoder-testbed
 
 [![Rust](https://github.com/drumath2237/rustwasm-decoder-testbed/actions/workflows/ci.yaml/badge.svg)](https://github.com/drumath2237/rustwasm-decoder-testbed/actions/workflows/ci.yaml)
+
+
+memo:
+
+```sh
+wasm-bindgen .\target\wasm32-unknown-unknown\debug\rustwasm-decoder-testbed.wasm --out-dir ./wasm --target bundler --out-name sog-wasm
+```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{fs::File, io::Read};
 
+use wasm_bindgen::prelude::wasm_bindgen;
 use zip::result::ZipError;
 
 use crate::{types::metajson::MetaJsonType, zip_test::SogFile};
@@ -7,6 +8,30 @@ use crate::{types::metajson::MetaJsonType, zip_test::SogFile};
 mod zip_test;
 
 mod types;
+
+#[wasm_bindgen(getter_with_clone)]
+pub struct FileInSog {
+    pub name: String,
+    pub data: Vec<u8>,
+}
+
+#[wasm_bindgen]
+pub fn zip_test_wasm(data: Vec<u8>) -> Vec<FileInSog> {
+    let sog_files = zip_test::zip_test(&data).unwrap();
+    sog_files
+        .iter()
+        .map(|file| match file {
+            SogFile::MetaJson(_meta) => FileInSog {
+                name: "meta.json".to_string(),
+                data: [].to_vec(),
+            },
+            SogFile::Image { filename, data } => FileInSog {
+                name: filename.to_string(),
+                data: data.to_vec(),
+            },
+        })
+        .collect()
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut file = File::open("./sample_data/pizza.sog").unwrap();
