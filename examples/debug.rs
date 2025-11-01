340c1a25221359f0fca83e47f8091146e6775ae3
drumath2237 <2237k.drumath@gmail.com>
2025-11-01 18:59:50 +0900

    :bug: image too large??

--- examples/debug.rs
+++ examples/debug.rs
@@ -1,6 +1,17 @@
+use image_webp::WebPDecoder;
 use rustwasm_decoder_testbed::test_func;
+use std::fs;
+use std::io::{Cursor, Read};
 
-fn main() {
-    let a = test_func();
-    println!("{}", &a);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut data = Vec::new();
+    fs::File::open("./sample_data/pizza/means_l.webp")?.read_to_end(&mut data)?;
+
+    let cursor = Cursor::new(data);
+    let mut decoder = WebPDecoder::new(cursor)?;
+
+    let mut pixels = Vec::new();
+    decoder.read_image(&mut pixels)?;
+
+    Ok(())
 }
